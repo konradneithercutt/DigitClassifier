f14340ebc6b1d5879daa5547aafb3c4635938347
Steven Marmorstein <stevenjmarmorstein@gmail.com>
2019-08-11 21:29:05 -0700

    Started interface.

--- NNGUI.py
+++ NNGUI.py
@@ -3,10 +3,22 @@ from tkinter import ttk
 
 class Classifier(object):
 
+    canvas = None
+
+
     def __init__(self):
         root = Tk()
-        ttk.Button(root, text="Hello World").grid()
+        root.title("Random Digit Display")
+        mainframe = ttk.Frame(root, padding="3 3 12 12")
+        mainframe.grid(column=0, row=0, sticky=(N, W, S, E))
+        root.columnconfigure(0, weight=1)
+        root.rowconfigure(0, weight=1)
+
+        ttk.Button(mainframe, text="Show Digit").grid(column=1, row=1, sticky=(N))
         root.mainloop()
+
+    def show_digit():
+        return
 
 
 if __name__ == "__main__":
